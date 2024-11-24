dff8f8cfce503dd915d9c7b1ee94b7dbd9d0e34a
mark1382 <66511007+mark1382@users.noreply.github.com>
2024-11-24 21:42:20 +0100

    Update foo.hpp

--- foo.hpp
+++ foo.hpp
@@ -4,9 +4,21 @@
 
 #include <list>
 #include <vector>
+#include <algorithm> // for std::reverse
 
 std::vector< char > foo(std::list< Human >& people)
 {
-    // Twoja implementacja tutaj
-    return {};
+    std::vector<char> result;
+    result.reserve(people.size()); // Rezerwacja miejsca w wektorze dla lepszej wydajności
+
+    // Iterujemy przez listę, modyfikujemy obiekty i budujemy wynikowy wektor
+    for (auto& person : people) {
+        person.birthday(); // Inkrementacja wieku
+        result.push_back(person.isMonster() ? 'y' : 'n'); // 'y' jeśli lubi psy/koty, 'n' jeśli nie
+    }
+
+    // Odwracamy kolejność wektora
+    std::reverse(result.begin(), result.end());
+
+    return result;
 }
